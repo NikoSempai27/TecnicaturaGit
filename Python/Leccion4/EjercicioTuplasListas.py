b922c0bd77aa0a907ef6c115fd8e688ac4434b7e
Pablo Nicolas Guala <claunikogual@gmail.com>
2022-09-10 20:39:33 -0300

    Ingresamos los ultimos ejercicios de Python

---
+++ Python/Leccion4/EjercicioTuplasListas.py
@@ -0,0 +1,16 @@
+# Dada la siguiente Tupla
+tupla = (13, 1, 8, 3, 2, 5, 8)
+# Crear una lista que solo contenga los numeros menores a 5
+# e imprima por consola [1, 3, 2]
+lista = [] # Definimos fla lista
+
+for elemento in tupla:
+    if elemento <5:
+        lista.append(elemento)
+print(lista)
+
+
+
+
+
+
--- Python/Leccion4/main.py
+++ Python/Leccion4/main.py
@@ -1,1 +1,31 @@
+# Lista Ariel Liliana Natalia Osvaldo
+
+nombres = ["Naty", "Osvaldo", "Lily", "Ariel"]
+print(nombres)
+print(nombres[0:2]) # Solo muestra el indice 0 y 1 pero no el 2
+# ir del inicio de la lista al indice (sin incluirlo)
+print(nombres[ :3]) # Indices a mostrar 0, 1, 2
+# Desde el ndice indicado hasta el final
+print(nombres[1: ])
+# Modificamos un valor
+nombres[2] = 'Liliana'
+nombres[0] = 'Natalia'
+print(nombres)
+# Iterar una lista
+for nombre in nombres: # nombre es singular, la lista es plural
+    print(nombre)
+else:
+    print('Se acabaron los elementos de la lista')
+
+# Preguntamos cuantos elementos tiene
+print(len(nombres)) # le pasamos como parametro la lista
+
+# Agregamos un elemento
+nombres.append('Marcelo')
+print(nombres)
+
+# Insertar un elemento en un indice especifico
+nombres.insert(1, 'Alberto')
+print(nombres)
+
 
