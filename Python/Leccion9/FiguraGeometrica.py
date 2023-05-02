467db5a83db483a2b9e56a6f29ce0045d359a783
Pablo Nicolas Guala <claunikogual@gmail.com>
2023-05-01 22:13:09 -0300

    Clase 13 de Python
    Hablamos de validaciones, Métodos encapsulados, validaciones en Setter, clases Abstractas y comienzo de contexto Estático

--- Python/Leccion9/FiguraGeometrica.py
+++ Python/Leccion9/FiguraGeometrica.py
@@ -1,7 +1,18 @@
-class FiguraGeometrica:
+from abc import  ABC, abstractmethod
+# ABC significa Abstract Base Class, convierte una clase en abstracta
+
+class FiguraGeometrica(ABC):
     def __init__(self, ancho, alto):
-        self._ancho = ancho
-        self._alto = alto
+        if self._validar_valores(ancho):
+            self._ancho = ancho
+        else:
+            self._ancho = 0
+            print(f'Valor erroneo para el ancho: {ancho}')
+        if self._validar_valores(alto):
+            self._alto = alto
+        else:
+            self._alto = 0
+            print(f'Valor erroneo para el alto: {alto}')
 
     @property
     def ancho(self):
@@ -9,7 +20,10 @@ class FiguraGeometrica:
 
     @ancho.setter
     def ancho(self, ancho):
-        self._ancho = ancho
+        if self._validar_valores(ancho):
+            self._ancho = ancho
+        else:
+            print(f'Valor erroneo para ancho: {ancho}')
 
     @property
     def alto(self):
@@ -17,7 +31,17 @@ class FiguraGeometrica:
 
     @alto.setter
     def alto(self, alto):
-        self._alto = alto
+        if self._validar_valores(alto):
+            self._alto = alto
+        else:
+            print(f'Valor erroneo para alto: {alto}')
+
+    @abstractmethod
+    def calcular_area(self):
+        pass
 
     def __str__(self):
         return f'FiguraGeometrica [Ancho: {self._ancho}, Alto: {self._alto} ]'
+
+    def _validar_valores(self, valor): # metodo encapsulado
+        return True if 0 < valor < 10 else False
--- Python/Leccion9/Test_Figura_Geometrica.py
+++ Python/Leccion9/Test_Figura_Geometrica.py
@@ -1,17 +1,25 @@
 from Cuadrado import Cuadrado
+from FiguraGeometrica import FiguraGeometrica
 from Rectangulo import Rectangulo
 
-cuadrado1 = Cuadrado(5, 'Azul')
-print(cuadrado1.ancho)
-print(cuadrado1.alto)
+print('Creacion de objeto clase Cuadrado' .center(50, '_'))
+cuadrado1 = Cuadrado(8, 'Azul')
+cuadrado1.alto = 7
+cuadrado1.ancho = 7
+# print(cuadrado1.ancho)
+# print(cuadrado1.alto)
 print(f'El cuadrado es de color: {cuadrado1.color}')
 print(f'Calculo del Area del cuadrado: {cuadrado1.calcular_area()}')
 
 # MRO = Method Resolution Order
-print(Cuadrado.mro())
+# print(Cuadrado.mro())
 
 print(cuadrado1)
-
-rectangulo1 = Rectangulo(3, 8, 'Amarillo')
+print('Creacion de objeto clase Rectangulo' .center(50, '_'))
+rectangulo1 = Rectangulo(3, 9, 'Amarillo')
+rectangulo1.ancho = 8
 print(f'Calcular el area del rectangulo: {rectangulo1.calcular_area()}')
 print(rectangulo1)
+
+# figura1 = FiguraGeometrica() No se puede instanciar, es abstracta
+
