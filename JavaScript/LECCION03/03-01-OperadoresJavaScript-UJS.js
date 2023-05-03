0676aad22933135f7da7f3e5197da6f6557e1501
Pablo Nicolas Guala <claunikogual@gmail.com>
2023-05-02 21:29:38 -0300

    Operadores en JavaScript y Ejercicios
    Ejercicios: Par o Impar, Mayor de Edad y Dentro del Rango

---
+++ JavaScript/LECCION03/03-01-OperadoresJavaScript-UJS.js
@@ -0,0 +1,27 @@
+//Ejercicio para encontrar numeros pares e impares
+let parImpar = 10;
+if(parImpar % 2 == 0){
+    console.log("Es un numero PAR");
+}
+else{
+    console.log("Es un numero IMPAR");
+}
+
+//Ejercicio: Es mayor de edad
+let edad = 18, adulto = 18;
+if(edad >= adulto){
+    console.log('Usted es una persona adulta');
+}
+else{
+    console.log('Usted es una persona menor de edad')
+}
+
+//Ejercicio dentro de un Rago
+let dentroRango = 5; //Aqui vamos a ir cambiando el valor
+let valMin =0, valMax = 10;
+if(dentroRango >= valMin && dentroRango <= valMax){
+    console.log('El valor esta dentro del Rango');
+}
+else{
+    console.log('El valor esta fuera del Rango')
+}
--- JavaScript/Leccion02/02-01-ConcatenacionDeCadenas.js
+++ JavaScript/Leccion02/02-01-ConcatenacionDeCadenas.js
@@ -13,3 +13,22 @@ console.log(juntos)
 
 nombre += apellido; //Tercera concatenacion usando el operador simplificado
 console.log(nombre);
+
+// Hoy ya no se usa var, se utiliza let y const
+let nombre2 = "Pedro";
+console.log(nombre2);
+
+const apellido2 = "Lepes";
+//apellido2 = "Peres"; una constante no puede ser modificada
+console.log(apellido2);
+
+let x, y; //Se pueden crear varias variables dentro de una misma linea
+x = 17, y = 21; //Se puede hacer asignaciones de varias variables dentro de una misma linea
+let z = x + y; //Se asigna el valor de la operacion
+console.log(z);
+
+let _1num = 31; //No utilizar numeros para iniciar el nombre de una variable
+let rompiendo = "rompe"; //No utilizar palabras reservadas para variables
+
+console.log(_1num);
+console.log(rompiendo);
